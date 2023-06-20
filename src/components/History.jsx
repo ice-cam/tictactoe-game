88692285c7734861eec58dcb4663cf821e798d5d
Komal <kshekhawat2011@gmail.com>
2023-06-20 10:19:09 +0530

    Added start new game button

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,21 +1,18 @@
-const History = ({ history }) => {
+const History = ({ history, moveTo, currentMove }) => {
   return (
     <div className="history-wrapper">
       <ul className="history">
-        {history.map(
-          (_,
-          index => (
-            <li key={index}>
-              <button
-                type="button"
-                className={`btn-move  ${currentMove === index ? 'active' : ''}`}
-                onClick={() => moveTo()}
-              >
-                {index === 0 ? `Go to game start` : `Go to move #${index}`}
-              </button>
-            </li>
-          ))
-        )}
+        {history.map((_, index) => (
+          <li key={index}>
+            <button
+              type="button"
+              className={`btn-move  ${currentMove === index ? 'active' : ''}`}
+              onClick={() => moveTo()}
+            >
+              {index === 0 ? `Go to game start` : `Go to move #${index}`}
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
